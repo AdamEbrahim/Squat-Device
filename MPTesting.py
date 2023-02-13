134886ef04fa5057b66858dc6efa4507651df83a
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2023-02-13 17:27:14 -0500

    added state transition diagram for squat and angle calculations maybe

--- MPTesting.py
+++ MPTesting.py
@@ -38,7 +38,9 @@ uprightAngle = True
 rightHipIndex = landmark_names.index('right_hip')
 rightKneeIndex = landmark_names.index('right_knee')
 rightShoulderIndex = landmark_names.index('right_shoulder')
+rightAnkleIndex = landmark_names.index('right_ankle')
 
+currentSquatState = 0 #0 = at top position, 1 = descent, 2 = at bottom, 3 = ascent
 while cv2.waitKey(1) != 27:
     has_frame, frame = cam.read()
     if has_frame != True:
@@ -47,10 +49,14 @@ while cv2.waitKey(1) != 27:
     frame.flags.writeable = False
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = model.process(frame)
+
+    frame.flags.writeable = True
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
     pose_landmarks = results.pose_world_landmarks
 
     if pose_landmarks is not None:
-      # Get landmarks.
+      # Get landmarks into np array
       frame_height, frame_width = frame.shape[0], frame.shape[1]
       pose_landmarks = np.array([[lmk.x * frame_width, lmk.y * frame_height, lmk.z * frame_width]
                                  for lmk in pose_landmarks.landmark], dtype=np.float32)
@@ -59,6 +65,8 @@ while cv2.waitKey(1) != 27:
     landmarks = np.copy(pose_landmarks)
     
     if landmarks.shape == (33, 3):
+        #check if hit depth based on rightHip and rightKnee:
+
         if landmarks[rightHipIndex].shape == (3,) and landmarks[rightKneeIndex].shape == (3,):    
         #for i in range(len(landmarks)):
         #print(i, landmarks[i])
@@ -69,19 +77,68 @@ while cv2.waitKey(1) != 27:
                 print("you have exited the bottom position")
                 atBottom = False
 
+        #check back angle (forward tilt) based on right shoulder and right hip:
         if landmarks[rightShoulderIndex].shape == (3,) and landmarks[rightHipIndex].shape == (3,):
+
             #back angle
-            yDist = landmarks[rightShoulderIndex][1] - landmarks[rightHipIndex][1]
-            xDist = landmarks[rightShoulderIndex][0] - landmarks[rightHipIndex][0]
-            angle = math.degrees(math.atan(yDist/xDist))
-            print(angle)
+            yDistBack = landmarks[rightShoulderIndex][1] - landmarks[rightHipIndex][1]
+            xDistBack = landmarks[rightShoulderIndex][0] - landmarks[rightHipIndex][0]
+            angleBack = round(math.degrees(math.atan(xDistBack/yDistBack)))
+            #print(angleBack)
             #if angle > 60 and uprightAngle:
+                
+            #draw back angle on the frame at the hip
+            hipPos = np.array(landmarks[rightHipIndex][0], landmarks[rightHipIndex][1])
+            hipVerticalLineEnd = np.array(landmarks[rightHipIndex][0], landmarks[rightHipIndex][1] + 5)
+            cv2.line(frame, tuple(hipPos), tuple(hipVerticalLineEnd), (255, 0, 0), 2)
+            cv2.putText(frame, str(angleBack), tuple(hipPos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+        #check upper leg angle (dist from parallel to ground) based on right hip and right knee:
+        if landmarks[rightHipIndex].shape == (3,) and landmarks[rightKneeIndex].shape == (3,):
+            yDistUpperLeg = landmarks[rightHipIndex][1] - landmarks[rightKneeIndex][1]
+            xDistUpperLeg = landmarks[rightKneeIndex][0] - landmarks[rightHipIndex][0]
+            angleUpperLeg = round(math.degrees(math.atan(yDistUpperLeg/xDistUpperLeg)))
+
+            kneePos = np.array(landmarks[rightKneeIndex][0], landmarks[rightKneeIndex][1])
+            kneeHorizontalLineEnd = np.array(landmarks[rightKneeIndex][0] - 5, landmarks[rightKneeIndex][1])
+            cv2.line(frame, tuple(kneePos), tuple(kneeHorizontalLineEnd), (255, 0, 0), 2)
+            cv2.putText(frame, str(angleUpperLeg), tuple(kneePos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+            #state transition diagram for wht position of the squat you are in
+            if angleUpperLeg > 70:
+                if currentSquatState == 0:
+                    print("still in top position of squat")
+                elif currentSquatState == 3:
+                    print("Completed rep")
+                    currentSquatState = 0
+            elif angleUpperLeg > 15: 
+                if currentSquatState == 0:
+                    print("descending")
+                    currentSquatState = 1
+                elif currentSquatState == 2:
+                    print("ascending")
+                    currentSquatState = 3
+            elif angleUpperLeg > -5:
+                if currentSquatState == 1:
+                    print("hit parallel")
+                    currentSquatState = 2
+            else:
+                print("deeper than parallel")
+
+
+        #check lower leg angle (forward tilt) based on right ankle and right knee:
+        if landmarks[rightAnkleIndex].shape == (3,) and landmarks[rightKneeIndex].shape == (3,):
+            yDistLowerLeg = landmarks[rightKneeIndex][1] - landmarks[rightAnkleIndex][1]
+            xDistLowerLeg = landmarks[rightShoulderIndex][0] - landmarks[rightHipIndex][0]
+            angleLowerLeg = round(math.degrees(math.atan(xDistLowerLeg/yDistLowerLeg)))
+
+            anklePos = np.array(landmarks[rightAnkleIndex][0], landmarks[rightAnkleIndex][1])
+            ankleVerticalLineEnd = np.array(landmarks[rightAnkleIndex][0], landmarks[rightAnkleIndex][1] + 5)
+            cv2.line(frame, tuple(anklePos), tuple(ankleVerticalLineEnd), (255, 0, 0), 2)
+            cv2.putText(frame, str(angleLowerLeg), tuple(anklePos), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
        
 
 
-
-    frame.flags.writeable = True
-    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
     drawLimbs.draw_landmarks(
         frame,
         results.face_landmarks,
@@ -95,6 +152,13 @@ while cv2.waitKey(1) != 27:
         mp_holistic.POSE_CONNECTIONS,
         landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style()
     )
+
+    #drawLimbs.plot_landmarks(
+        #results.pose_landmarks,
+        #mp_holistic.POSE_CONNECTIONS
+
+    #)
+
 
 
     cv2.imshow(windowName, cv2.flip(frame, 1))
