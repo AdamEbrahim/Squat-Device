e8c2162765bf6e595d4016c667c3239666162f62
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2023-08-20 00:22:00 -0500

    l

--- Atrocious_Code/MPTesting.py
+++ Atrocious_Code/MPTesting.py
@@ -5,33 +5,6 @@ import cv2
 import time
 from collections import deque
 
-#--CAMERA PIPELINE--#
-def gstreamer_pipeline(
-    sensor_id=0,
-    capture_width=1920,
-    capture_height=1080,
-    display_width=960,
-    display_height=540,
-    framerate=30,
-    flip_method=0,
-):
-    return (
-        "nvarguscamerasrc sensor-id=%d ! "
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (
-            sensor_id,
-            capture_width,
-            capture_height,
-            framerate,
-            flip_method,
-            display_width,
-            display_height,
-        )
-    )
 
 #--GLOBAL VARIABLES--#
 
@@ -433,9 +406,8 @@ if __name__ == "__main__":
 
     #initialize CV2 Video input and output
     camera_id = "/dev/video0"
-    cam = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
     #cam = cv2.VideoCapture(camera_id, cv2.CAP_V4L2)
-    #cam = cv2.VideoCapture(0)
+    cam = cv2.VideoCapture(0)
 
     cv2.waitKey(100)
     windowName = "Camera View"
