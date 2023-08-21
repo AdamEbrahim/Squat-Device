ec2d67dc16851ccc8d2a513775c930e9f420e3c0
AdamEbrahim <110650531+AdamEbrahim@users.noreply.github.com>
2023-08-20 22:32:39 -0500

    test

--- Atrocious_Code/cameraTesting.py
+++ Atrocious_Code/cameraTesting.py
@@ -2,7 +2,7 @@ import sys
 import cv2
 
 def read_cam():
-    cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080,format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw, format=(string)BGRx ! videoconvert !  appsink", cv2.CAP_GSTREAMER)
+    cap = cv2.VideoCapture("nvarguscamerasrc ! video/x-raw(memory:NVMM),format=NV12,width=640,height=480,framerate=30/1 ! nvvidconv ! video/x-raw,format=BGRx ! videoconvert ! video/x-raw,format=BGR ! appsink drop=1", cv2.CAP_GSTREAMER)
     if cap.isOpened():
         cv2.namedWindow("demo", cv2.WINDOW_AUTOSIZE)
         while True:
